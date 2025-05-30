6266703834d3ab1b2a7c8925059cd8e500a66fa6
pS <paloma.stucke@est.fi.uncoma.edu.ar>
2025-05-30 17:41:32 -0300

    Update Test_Empresa_Cable.php

--- Test_Empresa_Cable.php
+++ Test_Empresa_Cable.php
@@ -6,6 +6,9 @@ include_once 'cWeb.php';
 include_once 'Plan.php';
 include_once 'EmpresaCable.php';
 
+
+$miEmpresa= new EmpresaCable();
+
 $unCliente= new Cliente("Carla", "Lopez", "DNI", 21111111);
 $otroCliente= new Cliente("Juan", "Perez", "DNI", 18888888);
 $ultCliente= new Cliente("Noe", "Morales", "DNI", 33333333 );
@@ -16,6 +19,7 @@ $ultCanal= new Canal("infantil", 10, false);
 
 
 $unPlan= new Plan(1,[$unCanal,$otroCanal,$ultCanal], 10000);
+$otroPlan= new Plan(111, [$unCanal,$otroCanal,$ultCanal], 10000);
 
 $cont1= new Contrato(1,"05/05/2025", "04/06/2025", $unPlan, "al dia", 4200, true, $unCliente);
 $cont2= new ContratoWeb(2,"05/05/2025", "04/06/2025", $unPlan,"al dia", 4200, true,$otroCliente);
@@ -24,8 +28,6 @@ $con3= new ContratoWeb(3,"05/05/2025", "04/06/2025", $unPlan,"al dia", 4200, tru
 echo "Importe final primer contrato: $" . $cont1->calcularImporte();
 echo "Importe final primer contrato: $" . $cont2->calcularImporte();
 echo "Importe final primer contrato: $" . $cont3->calcularImporte();
-
-$miEmpresa= new EmpresaCable();
 
 $miEmpresa->incorporarPlan($unPlan);
 $miEmpresa->incorporarContrato($unPlan,$unCliente, "30/05/2025", "30/06/2025", false);
